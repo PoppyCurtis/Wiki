1afbd7dcb411f6a712c7864ecc9053fab7b27940
PoppyCurtis <poppy.curtis@gmail.com>
2019-04-23 21:23:42 +0100

    PUT a specific article and test via Postman

--- app.js
+++ app.js
@@ -59,12 +59,12 @@ app.route("/articles")
             }
         });
     });
-    app.route("/articles/:articletitle")
+app.route("/articles/:articletitle")
 
-   
-    .get(function(req, res) { 
-       
-        Article.findOne({title: req.params.articletitle}, function(err, result) {
+
+    .get(function (req, res) {
+
+        Article.findOne({ title: req.params.articletitle }, function (err, result) {
             if (result) {
                 res.send(result);
             }
@@ -72,9 +72,19 @@ app.route("/articles")
                 res.send("No articles were found");
             }
         });
+    })
+    .put(function (req, res) {
+        Article.update(
+            { title: req.params.articletitle },
+            { title: req.body.title, content: req.body.content },
+            { overwrite: true }, 
+            function (err, results) {
+                if(!err) {
+                    res.send("Successfully changed");
+                }
+            })
     });
-    //.post()
-    //.delete();
+//.delete();
 
 app.listen(3000, function (req, res) {
     console.log("Server running on Port 3000");
